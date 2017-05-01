4f55d1c50e0504e03bdab92e9c8e2d4ad9b61d1d
shubhangi-tandon <shubhi194@gmail.com>
2017-05-01 16:25:05 -0700

    move code into src folder

--- src/seq2seq.py
+++ src/seq2seq.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 import tensorflow as tf
 import  copy
-def pre():
+def read_from_csv():
     df = pd.DataFrame.from_csv(os.getcwd() + "/../friends.csv")
     print(len(df))
     X = list(df['utterance'])
@@ -15,7 +15,35 @@ def pre():
     #print(len(X))
     #print(len(Y))
     return (X, Y)
+
+def loadGloVe():
+    filename = 'glove.6B.50d.txt'
+    vocab = []
+    embd = []
+    file = open(filename,'r')
+    for line in file.readlines():
+        row = line.strip().split(' ')
+        vocab.append(row[0])
+        embd.append(row[1:])
+    print('Loaded GloVe!')
+    file.close()
+    return vocab,embd
+
+
+
+    pass
+
 def main():
-    (X,Y) =pre()
+    sess = tf.Session()
+    (X,Y) =read_from_csv()
+    vocab,embd = loadGloVe()
+    vocab_size = len(vocab)
+    embedding_dim = len(embd[0])
+    embedding = np.asarray(embd)
+    W = tf.Variable(tf.constant(0.0, shape=[vocab_size, embedding_dim]),
+                trainable=False, name="W")
+    embedding_placeholder = tf.placeholder(tf.float32, [vocab_size, embedding_dim])
+    embedding_init = W.assign(embedding_placeholder)
+    sess.run(embedding_init, feed_dict={embedding_placeholder: embedding})
 
 main()
