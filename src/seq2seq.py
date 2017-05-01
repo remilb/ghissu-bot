ee187cbe35873dffddd60fdee8ff10b34cab3ea8
shubhangi-tandon <shubhi194@gmail.com>
2017-05-01 15:46:07 -0700

    move code into src folder

---
+++ src/seq2seq.py
@@ -0,0 +1,21 @@
+
+import  pandas as pd
+import numpy as np
+import os
+import tensorflow as tf
+import  copy
+def pre():
+    df = pd.DataFrame.from_csv(os.getcwd() + "/../friends.csv")
+    print(len(df))
+    X = list(df['utterance'])
+    Y = copy.deepcopy(X)
+    X =X[:-1]
+    Y = Y[1:]
+
+    #print(len(X))
+    #print(len(Y))
+    return (X, Y)
+def main():
+    (X,Y) =pre()
+
+main()
