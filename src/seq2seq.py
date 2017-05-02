da9d36853da0eca6715f73c5b03e0725026f16d6
sharathts <ts.sharath18@gmail.com>
2017-05-01 19:37:33 -0700

    sentences mapped to vectors

--- src/seq2seq.py
+++ src/seq2seq.py
@@ -2,7 +2,7 @@ import  pandas as pd
 import numpy as np
 import os
 import tensorflow as tf
-import  copy
+import copy
 from tensorflow.contrib import learn
 
 def read_from_csv():
@@ -48,7 +48,6 @@ def main():
     vocab_size = len(vocab)
     embedding_dim = len(embd[0])
     embedding = np.asarray(embd)
-    print embedding[0], vocab[0]
     
     
     #init vocab processor
@@ -60,18 +59,20 @@ def main():
     
     print X[0]
     
-    
-#    TF variable (placeholder)
-    W = tf.Variable(tf.constant(0.0, shape=[vocab_size, embedding_dim]),
-                trainable=False, name="W")
-    embedding_placeholder = tf.placeholder(tf.float32, [vocab_size, embedding_dim])
-    embedding_init = W.assign(embedding_placeholder)
-    tf.nn.embedding_lookup(W, X)    
-    
-    
-    sess.run(embedding_init, feed_dict={embedding_placeholder: embedding})
+    with tf.Session() as sess:
+        
+    #    TF variable (placeholder)
+        W = tf.Variable(tf.constant(0.0, shape=[vocab_size, embedding_dim]),
+                    trainable=False, name="W")
+        embedding_placeholder = tf.placeholder(tf.float32, [vocab_size, embedding_dim])
+        embedding_init = W.assign(embedding_placeholder)
+        
+        req_embedded = tf.nn.embedding_lookup(W, X)
+        
+#        Call the session
+        sess.run(embedding_init, feed_dict={embedding_placeholder: embedding})
 
-    print X[0]
-    var = W.eval(session=sess)
-    print var
+        print(req_embedded.get_shape)
+        print(req_embedded.eval()[0])
+
 main()
