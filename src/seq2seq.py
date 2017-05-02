a5ac01fa11e8a64b766a526b22c8328eb7d1c3da
sharathts <ts.sharath18@gmail.com>
2017-05-01 19:17:08 -0700

    Embeddings stage

--- src/seq2seq.py
+++ src/seq2seq.py
@@ -3,29 +3,26 @@ import numpy as np
 import os
 import tensorflow as tf
 import  copy
-<<<<<<< HEAD
+from tensorflow.contrib import learn
 
-def pre():
-
-=======
 def read_from_csv():
->>>>>>> 4f55d1c50e0504e03bdab92e9c8e2d4ad9b61d1d
-    df = pd.DataFrame.from_csv(os.getcwd() + "/../friends.csv")
 
+    df = pd.DataFrame.from_csv("/home/sharath/programming/friends.csv")
+    df = df.dropna(subset = ['utterance'])
     print(len(df))
     X = list(df['utterance'])
+    
     Y = copy.deepcopy(X)
-    X =X[:-1]
+    X = X[:-1]
     Y = Y[1:]
 
     #print(len(X))
     #print(len(Y))
     return (X, Y)
 
-<<<<<<< HEAD
-=======
-def loadGloVe():
-    filename = 'glove.6B.50d.txt'
+def loadGloVe(filename):
+
+#    pwd = os.getcwd()
     vocab = []
     embd = []
     file = open(filename,'r')
@@ -35,24 +32,46 @@ def loadGloVe():
         embd.append(row[1:])
     print('Loaded GloVe!')
     file.close()
-    return vocab,embd
+    return vocab, embd
 
 
-
-    pass
-
->>>>>>> 4f55d1c50e0504e03bdab92e9c8e2d4ad9b61d1d
 def main():
+
     sess = tf.Session()
-    (X,Y) =read_from_csv()
-    vocab,embd = loadGloVe()
+    (X,Y) = read_from_csv()
+    max_sentence_length = 20
+
+    filename = '/home/sharath/programming/glove.6B.100d.txt'
+
+    vocab, embd = loadGloVe(filename)
+    
     vocab_size = len(vocab)
     embedding_dim = len(embd[0])
     embedding = np.asarray(embd)
+    print embedding[0], vocab[0]
+    
+    
+    #init vocab processor
+    vocab_processor = learn.preprocessing.VocabularyProcessor(max_sentence_length)
+    #fit the vocab from glove
+    pretrain = vocab_processor.fit(vocab)
+    #transform inputs
+    X = np.array(list(vocab_processor.transform(X)))
+    
+    print X[0]
+    
+    
+#    TF variable (placeholder)
     W = tf.Variable(tf.constant(0.0, shape=[vocab_size, embedding_dim]),
                 trainable=False, name="W")
     embedding_placeholder = tf.placeholder(tf.float32, [vocab_size, embedding_dim])
     embedding_init = W.assign(embedding_placeholder)
+    tf.nn.embedding_lookup(W, X)    
+    
+    
     sess.run(embedding_init, feed_dict={embedding_placeholder: embedding})
 
+    print X[0]
+    var = W.eval(session=sess)
+    print var
 main()
