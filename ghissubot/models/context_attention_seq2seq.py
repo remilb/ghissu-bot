7e3d279581b57bf0f0b5595ed466b6e1631abfa2
Ryan <rbauer@ucsc.edu>
2017-05-24 15:18:00 -0700

    Not actually sure what's in this commit. oops

---
+++ ghissubot/models/context_attention_seq2seq.py
@@ -0,0 +1,63 @@
+"""
+Contextual Sequence to Sequence model with attention
+"""
+from pydoc import locate
+
+import tensorflow as tf
+
+from seq2seq import decoders
+from ghissubot.models.context_seq2seq import ContextSeq2Seq
+
+
+class AttentionSeq2SeqWithContext(ContextSeq2Seq):
+    """Sequence2Sequence model with attention mechanisms that also attends
+    over a representation of context over prior utterances"""
+
+    def __init__(self, params, mode, name="att_seq2seq"):
+        super(ContextSeq2Seq, self).__init__(params, mode, name)
+
+    @staticmethod
+    def default_params():
+        params = ContextSeq2Seq.default_params().copy()
+        params.update({
+            "attention.class": "AttentionLayerBahdanau",
+            "attention.params": {},  # Arbitrary attention layer parameters
+            "bridge.class": "seq2seq.models.bridges.ZeroBridge",
+            "encoder.class": "seq2seq.encoders.BidirectionalRNNEncoder",
+            "encoder.params": {},  # Arbitrary parameters for the encoder
+            "decoder.class": "seq2seq.decoders.AttentionDecoder",
+            "decoder.params": {}  # Arbitrary parameters for the decoder
+        })
+        return params
+
+
+  def _create_decoder(self, encoder_output, features, _labels):
+      #Get attention function (Bahadnau or Luong)
+    attention_class = locate(self.params["attention.class"]) or \
+      getattr(decoders.attention, self.params["attention.class"])
+    attention_layer = attention_class(
+        params=self.params["attention.params"], mode=self.mode)
+
+    # If the input sequence is reversed we also need to reverse
+    # the attention scores.
+    reverse_scores_lengths = None
+    if self.params["source.reverse"]:
+      reverse_scores_lengths = features["source_len"]
+      if self.use_beam_search:
+        reverse_scores_lengths = tf.tile(
+            input=reverse_scores_lengths,
+            multiples=[self.params["inference.beam_search.beam_width"]])
+
+    #TODO: Make sure this is joining along the correct dimension
+    attention_values = tf.concat([encoder_output.context_outputs, encoder_output.attention_values], 0)
+    attention_keys = tf.concat([encoder_output.context_outputs, encoder_output.outputs], 0)
+    attention_values_length = tf.add(encoder_output.attention_values_length, tf.constant(1))
+    return self.decoder_class(
+        params=self.params["decoder.params"],
+        mode=self.mode,
+        vocab_size=self.target_vocab_info.total_size,
+        attention_values=attention_values,
+        attention_values_length=attention_values_length,
+        attention_keys=attention_keys,
+        attention_fn=attention_layer,
+        reverse_scores_lengths=reverse_scores_lengths)
