676c432c370f051a0e748ee61fad67baf18139c6
Ryan <rbauer@ucsc.edu>
2017-05-27 19:29:01 -0700

    Fix some merge conflicts

--- ghissubot/cnn_classification/text_cnn2.py
+++ ghissubot/cnn_classification/text_cnn2.py
@@ -13,7 +13,7 @@ class TextCNN(Configurable):
 
             "embedding.size": 128,
             "embedding.init_scale": 0.04,
-            "filter_sizes": list(map(int, "3,4,5".split(","))),
+            "filter_sizes": list(map(int, "3,4,5,6,8".split(","))),
             "num_filters": 128,
             "dropout_keep_prob": 1.0,
             "l2_reg_lambda": 0.0,
@@ -36,7 +36,7 @@ class TextCNN(Configurable):
     def __init__(self, params , mode, name = ""):
         '''cnn_source.max_seq_len, num_classes, vocab_size,
         embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0):'''
-        with tf.variable_scope("context_restore_prefix"):
+        with tf.variable_scope("model/att_seq2seq/encode_context/conv_context_encoder/context_restore_prefix"):
             self.name = name
             super(TextCNN, self).__init__(params, mode)
 
@@ -68,87 +68,88 @@ class TextCNN(Configurable):
             # Keeping track of l2 regularization loss (optional)
             l2_loss = tf.constant(0.0)
             #self.source_vocab_to_id = tf.placeholder(dtype= tf.contrib.lookup.HashTable, name="source_vocab_to_id")
-            with tf.variable_scope("vocab"):
-                '''preprocess code '''
-                source_vocab_to_id, source_id_to_vocab, source_word_to_count, _ = \
-                  vocab.create_vocabulary_lookup_table(self.source_vocab_info.path)
+            with tf.variable_scope("our_special_prefix"):
+                with tf.variable_scope("vocab"):
+                    '''preprocess code '''
+                    source_vocab_to_id, source_id_to_vocab, source_word_to_count, _ = \
+                      vocab.create_vocabulary_lookup_table(self.source_vocab_info.path)
 
-                features = source_vocab_to_id.lookup(tf.identity(self.input_x))
-                # features = source_vocab_to_id.lookup(input_x)
-                print("printing features shape", features.shape)
+                    features = source_vocab_to_id.lookup(tf.identity(self.input_x))
+                    # features = source_vocab_to_id.lookup(input_x)
+                    print("printing features shape", features.shape)
 
 
-            # Add vocab tables to graph collection so that we can access them in
-            # other places.
-            #todo we probably need to remove this
-            '''
-            graph_utils.add_dict_to_collection({
-                "cnn_source_vocab_to_id": self.source_vocab_to_id,
-                "cnn_source_id_to_vocab": source_id_to_vocab,
-                "cnn_source_word_to_count": source_word_to_count,
-            }, "cnn_vocab_tables")
-            #print(source_vocab_to_id.shape)'''
+                # Add vocab tables to graph collection so that we can access them in
+                # other places.
+                #todo we probably need to remove this
+                '''
+                graph_utils.add_dict_to_collection({
+                    "cnn_source_vocab_to_id": self.source_vocab_to_id,
+                    "cnn_source_id_to_vocab": source_id_to_vocab,
+                    "cnn_source_word_to_count": source_word_to_count,
+                }, "cnn_vocab_tables")
+                #print(source_vocab_to_id.shape)'''
 
-            '''end of preprocess code '''
-            '''------------------------------------------------------------------'''
+                '''end of preprocess code '''
+                '''------------------------------------------------------------------'''
 
-            '''source encoding definition'''
-            self.source_embedding = tf.get_variable(
-                name="cnn_W",
-                shape=[self.source_vocab_info.total_size, self.params["embedding.size"]],
-                initializer=tf.random_uniform_initializer(
-                    -self.params["embedding.init_scale"],
-                    self.params["embedding.init_scale"]))
-            '''end of source encoding'''
-            '''------------------------------------------------------------------'''
+                '''source encoding definition'''
+                self.source_embedding = tf.get_variable(
+                    name="cnn_W",
+                    shape=[self.source_vocab_info.total_size, self.params["embedding.size"]],
+                    initializer=tf.random_uniform_initializer(
+                        -self.params["embedding.init_scale"],
+                        self.params["embedding.init_scale"]))
+                '''end of source encoding'''
+                '''------------------------------------------------------------------'''
 
-            '''encode method'''
-            self.embedded_chars = tf.nn.embedding_lookup(self.source_embedding,
-                                                         features)
-                #self.embedded_chars = tf.nn.embedding_lookup(self.W, input_x)
-            self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
-            '''end of encode method'''
-            '''------------------------------------------------------------------'''
+                '''encode method'''
+                self.embedded_chars = tf.nn.embedding_lookup(self.source_embedding,
+                                                             features)
+                    #self.embedded_chars = tf.nn.embedding_lookup(self.W, input_x)
+                self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
+                '''end of encode method'''
+                '''------------------------------------------------------------------'''
 
-            # Create a convolution + maxpool layer for each filter size
-            pooled_outputs = []
-            for i, filter_size in enumerate(self.params["filter_sizes"]):
-                with tf.variable_scope("conv-maxpool-%s" % filter_size):
-                    # Convolution Layer
-                        #filter_shape = [filter_size, embedding_size, 1, num_filters]
-                    filter_shape = [filter_size, self.params["embedding.size"], 1, self.params["num_filters"]]
-                    W = tf.get_variable(name="W", initializer=tf.truncated_normal(filter_shape, stddev=0.1))
-                    b = tf.get_variable(name="b", initializer=tf.constant(0.1, shape=[self.params["num_filters"]]))
-                    conv = tf.nn.conv2d(
-                        self.embedded_chars_expanded,
-                        W,
-                        strides=[1, 1, 1, 1],
-                        padding="VALID",
-                        name="conv")
-                    # Apply nonlinearity
-                    h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-                    # Maxpooling over the outputs
-                    pooled = tf.nn.max_pool(
-                        h,
-                            ksize=[1, self.params["cnn_source.max_seq_len"] - filter_size + 1, 1, 1],
-                        strides=[1, 1, 1, 1],
-                        padding='VALID',
-                        name="pool")
-                    pooled_outputs.append(pooled)
+                # Create a convolution + maxpool layer for each filter size
+                pooled_outputs = []
+                for i, filter_size in enumerate(self.params["filter_sizes"]):
+                    with tf.variable_scope("conv-maxpool-%s" % filter_size):
+                        # Convolution Layer
+                            #filter_shape = [filter_size, embedding_size, 1, num_filters]
+                        filter_shape = [filter_size, self.params["embedding.size"], 1, self.params["num_filters"]]
+                        W = tf.get_variable(name="W", initializer=tf.truncated_normal(filter_shape, stddev=0.1))
+                        b = tf.get_variable(name="b", initializer=tf.constant(0.1, shape=[self.params["num_filters"]]))
+                        conv = tf.nn.conv2d(
+                            self.embedded_chars_expanded,
+                            W,
+                            strides=[1, 1, 1, 1],
+                            padding="VALID",
+                            name="conv")
+                        # Apply nonlinearity
+                        h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+                        # Maxpooling over the outputs
+                        pooled = tf.nn.max_pool(
+                            h,
+                                ksize=[1, self.params["cnn_source.max_seq_len"] - filter_size + 1, 1, 1],
+                            strides=[1, 1, 1, 1],
+                            padding='VALID',
+                            name="pool")
+                        pooled_outputs.append(pooled)
 
-            # Combine all the pooled features
-            num_filters_total = self.params["num_filters"] * len(self.params["filter_sizes"])
-            print(self.params["num_filters"], len(self.params["filter_sizes"]))
-            self.h_pool = tf.concat(pooled_outputs, 3)
-            print(num_filters_total)
-            print(self.h_pool.shape)
-            self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total], name="dummy")
+                # Combine all the pooled features
+                num_filters_total = self.params["num_filters"] * len(self.params["filter_sizes"])
+                print(self.params["num_filters"], len(self.params["filter_sizes"]))
+                self.h_pool = tf.concat(pooled_outputs, 3)
+                print(num_filters_total)
+                print(self.h_pool.shape)
+                self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total], name="dummy")
 
-            with tf.variable_scope("dropout_flatten"):
-                self.h_pool_flat_dropout = tf.nn.dropout(self.h_pool_flat, self.dropout_keep_prob)
+                with tf.variable_scope("dropout_flatten"):
+                    self.h_pool_flat_dropout = tf.nn.dropout(self.h_pool_flat, self.dropout_keep_prob)
 
-            self.feed_forward_layer = tf.contrib.layers.fully_connected(inputs=self.h_pool_flat_dropout, num_outputs=params["context_size"], scope="hidden_context_layer")
-            # Add dropout
+                self.feed_forward_layer = tf.contrib.layers.fully_connected(inputs=self.h_pool_flat_dropout, num_outputs=params["context_size"], scope="hidden_context_layer")
+                # Add dropout
 
             with tf.variable_scope("dropout_fully_connected"):
                 self.h_drop = tf.nn.dropout(self.feed_forward_layer, self.dropout_keep_prob)
--- ghissubot/encoders/conv_context_encoder.py
+++ ghissubot/encoders/conv_context_encoder.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import tensorflow as tf
 
 from seq2seq.encoders.encoder import Encoder, EncoderOutput
+from seq2seq.data import vocab
 
 
 class ConvContextEncoder(Encoder):
@@ -15,7 +16,7 @@ class ConvContextEncoder(Encoder):
       super(ConvContextEncoder, self).__init__(params, mode, name)
       if self.params["metagraph_filename"] == "":
           raise ValueError("Must provide metagraph file to load for {}!".format(self.__class__.__name__))
-      if self.params["max_sequence_length"] == 0:
+      if self.params["cnn_source.max_seq_len"] == 0:
           raise ValueError("Must provide max sequence length for {}!".format(self.__class__.__name__))
       if self.params["input_name"] == "":
           raise ValueError("Must provide name of input tensor for {}!".format(self.__class__.__name__))
@@ -23,52 +24,93 @@ class ConvContextEncoder(Encoder):
           raise ValueError("Must provide name for output (hidden repr tensor) for {}!".format(self.__class__.__name__))
       if self.params["naming_prefix"] == "":
           raise ValueError("Must provide naming prefix used for loaded graph in class {}!".format(self.__class__.__name__))
+      self.special_source_vocab_info = vocab.get_vocab_info(self.params["vocab_path"])
 
   @staticmethod
   def default_params():
     return {
-        "embedding_dim": 128,
+        "embedding.size": 128,
+        "embedding.init_scale": 0.04,
+        "filter_sizes": list(map(int, "3,4,5,6,8".split(","))),
+        "num_filters": 128,
+        "dropout_keep_prob": 1.0,
+        "l2_reg_lambda": 0.0,
         "metagraph_dir": "",
         "metagraph_filename": "",
-        "max_sequence_length": 0,
+        "cnn_source.max_seq_len": 30,
         "vocab_size": 20816,
         "input_name": "",
         "output_name": "",
         "output_layer_size": 0,
         "naming_prefix": "",
         "freeze_graph": True,
-        "padding_token": "ENDPADDING"
+        "padding_token": "ENDPADDING",
+        "vocab_path": ""
     }
 
   def encode(self, inputs, sequence_length, **kwargs):
         #TODO: This is where we need to import metagraph and hook into it
+        with tf.variable_scope("context_restore_prefix/our_special_prefix"):
+            with tf.variable_scope("vocab"):
+                '''preprocess code '''
+                source_vocab_to_id, source_id_to_vocab, source_word_to_count, _ = \
+                    vocab.create_vocabulary_lookup_table(self.special_source_vocab_info.path)
 
-        metagraph_file = self.params["metagraph_dir"] + '/' + self.params["metagraph_filename"]
+                features = source_vocab_to_id.lookup(tf.identity(inputs))
 
-        #TODO: Might need to make sure sequence lengths are all in order
-        # Inspiration from this?
-        # Slice source to max_len
-        if self.params["max_sequence_length"] is not None:
-            inputs = inputs[:, :self.params["max_sequence_length"]]
-            sequence_length = tf.minimum(sequence_length, self.params["max_sequence_length"])
+            '''source encoding definition'''
+            source_embedding = tf.get_variable(
+                name="cnn_W",
+                shape=[self.special_source_vocab_info.total_size, self.params["embedding.size"]],
+                initializer=tf.random_uniform_initializer(
+                    -self.params["embedding.init_scale"],
+                    self.params["embedding.init_scale"]))
+            '''end of source encoding'''
+            '''------------------------------------------------------------------'''
 
-        #TODO: Bind input tensors (inputs) to the input tensor of loaded subgraph
-        input_tensor_name = self.params["naming_prefix"] + '/' + self.params["input_name"] + ':0'
-        dropout_name = self.params["naming_prefix"] + '/' + "dropout_keep_prob"
-        input_y_name = self.params["naming_prefix"] + '/' + "input_y"
+            '''encode method'''
+            embedded_chars = tf.nn.embedding_lookup(source_embedding,
+                                                         features)
+            # self.embedded_chars = tf.nn.embedding_lookup(self.W, input_x)
+            embedded_chars_expanded = tf.expand_dims(embedded_chars, -1)
+            '''end of encode method'''
+            '''------------------------------------------------------------------'''
 
-        input_map = {input_tensor_name: inputs,
-                     dropout_name: tf.constant(1.0),
-                     input_y_name: tf.constant(0, shape=(64, 10))}
+            # Create a convolution + maxpool layer for each filter size
+            pooled_outputs = []
+            for i, filter_size in enumerate(self.params["filter_sizes"]):
+                with tf.variable_scope("conv-maxpool-%s" % filter_size):
+                    # Convolution Layer
+                    # filter_shape = [filter_size, embedding_size, 1, num_filters]
+                    filter_shape = [filter_size, self.params["embedding.size"], 1, self.params["num_filters"]]
+                    W = tf.get_variable(name="W", initializer=tf.truncated_normal(filter_shape, stddev=0.1))
+                    b = tf.get_variable(name="b", initializer=tf.constant(0.1, shape=[self.params["num_filters"]]))
+                    conv = tf.nn.conv2d(
+                        embedded_chars_expanded,
+                        W,
+                        strides=[1, 1, 1, 1],
+                        padding="VALID",
+                        name="conv")
+                    # Apply nonlinearity
+                    h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+                    # Maxpooling over the outputs
+                    pooled = tf.nn.max_pool(
+                        h,
+                        ksize=[1, self.params["cnn_source.max_seq_len"] - filter_size + 1, 1, 1],
+                        strides=[1, 1, 1, 1],
+                        padding='VALID',
+                        name="pool")
+                    pooled_outputs.append(pooled)
 
-        print(eval())
+            # Combine all the pooled features
+            num_filters_total = self.params["num_filters"] * len(self.params["filter_sizes"])
+            h_pool = tf.concat(pooled_outputs, 3)
+            h_pool_flat = tf.reshape(h_pool, [-1, num_filters_total], name="dummy")
 
-        # Now import metagraph, remapping our inputs to the appropriate place
-        tf.train.import_meta_graph(metagraph_file, input_map=input_map)
-        current_graph = tf.get_default_graph()
 
-        output_layer_name = self.params["naming_prefix"] + '/' + self.params["output_name"] + ':0'
-        context_vector = current_graph.get_tensor_by_name(output_layer_name)
+            context_vector = tf.contrib.layers.fully_connected(inputs=h_pool_flat,
+                                                                        num_outputs=512,
+                                                                        scope="hidden_context_layer")
 
         #TODO: We need to make sure to freeze the output tensor so that gradients don't flow
         if self.params["freeze_graph"]:
