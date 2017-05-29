2e2579d3cda8a255b7dce2f6b94868a9d809106b
Ryan Bauer <ryanemilbauer@gmail.com>
2017-05-29 03:40:26 -0700

    Modified conv_context_encoder.py so that Context Encoder to load frozen subgraph
    Utilize freeze_graph.py from tf utils to take a checkpoint and graphdef
    protobuf (different from metagraph) from the cnn encoder, and produce a
    frozen graphdef of the cnn with only nodes necesarry for inference and all
    variables replaced with constant weights from checkpoint. Unfortunately,
    for this to work I had to solve a heinous bug in the logic for
    freeze_graph to ensure that nodes pertaining to lookup table initializers
    (in this case for vocab) were kept. Once that was done, to ensure that the
    lookup tables are initialized, they are added to the global collection
    TABLE_INITIALIZERS so that the MonitoredSession initializes them. Yassss

--- ghissubot/encoders/conv_context_encoder.py
+++ ghissubot/encoders/conv_context_encoder.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
+from tensorflow.python.platform import gfile
 
 from seq2seq.encoders.encoder import Encoder, EncoderOutput
 from seq2seq.data import vocab
@@ -14,109 +15,74 @@ class ConvContextEncoder(Encoder):
 
   def __init__(self, params, mode, name="conv_context_encoder"):
       super(ConvContextEncoder, self).__init__(params, mode, name)
-      if self.params["metagraph_filename"] == "":
-          raise ValueError("Must provide metagraph file to load for {}!".format(self.__class__.__name__))
-      if self.params["cnn_source.max_seq_len"] == 0:
+      if self.params["frozen_graph_filename"] == "":
+          raise ValueError("Must provide frozen graph protobuf file to load for {}!".format(self.__class__.__name__))
+      if self.params["max_sequence_len"] == 0:
           raise ValueError("Must provide max sequence length for {}!".format(self.__class__.__name__))
-      if self.params["input_name"] == "":
+      if self.params["input_tensor_name"] == "":
           raise ValueError("Must provide name of input tensor for {}!".format(self.__class__.__name__))
-      if self.params["output_name"] == "":
+      if self.params["output_tensor_name"] == "":
           raise ValueError("Must provide name for output (hidden repr tensor) for {}!".format(self.__class__.__name__))
       if self.params["naming_prefix"] == "":
           raise ValueError("Must provide naming prefix used for loaded graph in class {}!".format(self.__class__.__name__))
-      self.special_source_vocab_info = vocab.get_vocab_info(self.params["vocab_path"])
 
   @staticmethod
   def default_params():
     return {
         "embedding.size": 128,
-        "embedding.init_scale": 0.04,
-        "filter_sizes": list(map(int, "3,4,5,6,8".split(","))),
-        "num_filters": 128,
-        "dropout_keep_prob": 1.0,
-        "l2_reg_lambda": 0.0,
-        "metagraph_dir": "",
-        "metagraph_filename": "",
-        "cnn_source.max_seq_len": 30,
+        "frozen_graph_dir": "",
+        "frozen_graph_filename": "",
+        "max_sequence_len": 0,
         "vocab_size": 20816,
-        "input_name": "",
-        "output_name": "",
-        "output_layer_size": 0,
         "naming_prefix": "",
-        "freeze_graph": True,
-        "padding_token": "ENDPADDING",
-        "vocab_path": ""
+        "input_tensor_name": "",
+        "output_tensor_name": "",
+        "dropout_prob_name": "",
+        "output_layer_size": 0,
     }
 
   def encode(self, inputs, sequence_length, **kwargs):
         #TODO: This is where we need to import metagraph and hook into it
-        with tf.variable_scope("context_restore_prefix/our_special_prefix"):
-            with tf.variable_scope("vocab"):
-                '''preprocess code '''
-                source_vocab_to_id, source_id_to_vocab, source_word_to_count, _ = \
-                    vocab.create_vocabulary_lookup_table(self.special_source_vocab_info.path)
 
-                features = source_vocab_to_id.lookup(tf.identity(inputs))
+        frozen_graph_file = self.params["frozen_graph_dir"] + '/' + self.params["frozen_graph_filename"]
 
-            '''source encoding definition'''
-            source_embedding = tf.get_variable(
-                name="cnn_W",
-                shape=[self.special_source_vocab_info.total_size, self.params["embedding.size"]],
-                initializer=tf.random_uniform_initializer(
-                    -self.params["embedding.init_scale"],
-                    self.params["embedding.init_scale"]))
-            '''end of source encoding'''
-            '''------------------------------------------------------------------'''
+        #TODO: Might need to make sure sequence lengths are all in order
+        if self.params["max_sequence_len"] is not None:
+            inputs = inputs[:, :self.params["max_sequence_len"]]
+            sequence_length = tf.minimum(sequence_length, self.params["max_sequence_len"])
 
-            '''encode method'''
-            embedded_chars = tf.nn.embedding_lookup(source_embedding,
-                                                         features)
-            # self.embedded_chars = tf.nn.embedding_lookup(self.W, input_x)
-            embedded_chars_expanded = tf.expand_dims(embedded_chars, -1)
-            '''end of encode method'''
-            '''------------------------------------------------------------------'''
+        # Now we have to read in binary protobuf for frozen graph, then parse it to a GraphDef
+        with gfile.FastGFile(frozen_graph_file, 'rb') as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
 
-            # Create a convolution + maxpool layer for each filter size
-            pooled_outputs = []
-            for i, filter_size in enumerate(self.params["filter_sizes"]):
-                with tf.variable_scope("conv-maxpool-%s" % filter_size):
-                    # Convolution Layer
-                    # filter_shape = [filter_size, embedding_size, 1, num_filters]
-                    filter_shape = [filter_size, self.params["embedding.size"], 1, self.params["num_filters"]]
-                    W = tf.get_variable(name="W", initializer=tf.truncated_normal(filter_shape, stddev=0.1))
-                    b = tf.get_variable(name="b", initializer=tf.constant(0.1, shape=[self.params["num_filters"]]))
-                    conv = tf.nn.conv2d(
-                        embedded_chars_expanded,
-                        W,
-                        strides=[1, 1, 1, 1],
-                        padding="VALID",
-                        name="conv")
-                    # Apply nonlinearity
-                    h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-                    # Maxpooling over the outputs
-                    pooled = tf.nn.max_pool(
-                        h,
-                        ksize=[1, self.params["cnn_source.max_seq_len"] - filter_size + 1, 1, 1],
-                        strides=[1, 1, 1, 1],
-                        padding='VALID',
-                        name="pool")
-                    pooled_outputs.append(pooled)
+        # Define our input map and output tensors of interest
+        input_tensor_name = self.params["naming_prefix"] + '/' + self.params["input_tensor_name"] + ':0'
+        input_map = {input_tensor_name: inputs}
+        output_tensors = [self.params["naming_prefix"] + '/' + self.params["output_tensor_name"]]
 
-            # Combine all the pooled features
-            num_filters_total = self.params["num_filters"] * len(self.params["filter_sizes"])
-            h_pool = tf.concat(pooled_outputs, 3)
-            h_pool_flat = tf.reshape(h_pool, [-1, num_filters_total], name="dummy")
+        # Also want to return the table initializer op
+        output_tensors.append("ghissubot_subgraph/init_all_tables")
 
+        # We also need to nullify the dropout layer
+        dropout_name = self.params["naming_prefix"] + '/' + self.params["dropout_prob_name"]
+        input_map[dropout_name] = tf.constant(1.0)
 
-            context_vector = tf.contrib.layers.fully_connected(inputs=h_pool_flat,
-                                                                        num_outputs=512,
-                                                                        scope="hidden_context_layer")
+        # Now we import the above graph edf, remapping our inputs to the appropriate nodes
+        # Note we index to zero to get the single output tensor we are requesting, as this returns a list
+        # It also returns an Operation, so we grab its outputs property to get a tensor
+        subgraph_nodes = tf.import_graph_def(graph_def, input_map=input_map, return_elements=output_tensors)
+        context_vector = subgraph_nodes[0].outputs[0]
+        table_initializer = subgraph_nodes[1]
 
-        #TODO: We need to make sure to freeze the output tensor so that gradients don't flow
-        if self.params["freeze_graph"]:
-            context_vector = tf.stop_gradient(context_vector)
+        # Finally, we need to add the table initializers in the imported subgraph to the global table initializers collection
+        # This ensures that the vocab lookup tables in the subgraph get initialized. This is so fucked up
+        tf.add_to_collection(tf.GraphKeys.TABLE_INITIALIZERS, table_initializer)
 
-        # logits_vector = current_graph.get_operation_by_name("context_restore_prefix/loss/SoftmaxCrossEntropyWithLogits")
-        # tf.assign(logits_vector, tf.constant(0, shape=(64,10)))
+        variable_names = [tensor.name for tensor in tf.get_default_graph().as_graph_def().node]
+        for name in variable_names: print(name + "\n")
+
+        # Now we need to get a handle to the outputs
+
         # Note that we don't return an EncoderOutput like the other classes, just the context vector
         return context_vector
